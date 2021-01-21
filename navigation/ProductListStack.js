ee3bed7e3e7023f160b52260e60487409f3a3bf2
Anand-oops <anand.pandey6599@gmail.com>
2021-01-21 20:42:14 +0530

    ProductListStack Created

---
+++ navigation/ProductListStack.js
@@ -0,0 +1,27 @@
+import { createStackNavigator } from "react-navigation-stack";
+import Header from '../shared/Header';
+import React from 'react';
+import ProductListScreen from "../screens/ProductsListScreen";
+import ProductDetailsScreen from '../screens/ProductDetailsScreen'
+
+const screens = {
+    ProductList: {
+        screen: ProductListScreen, navigationOptions: ({ navigation }) => {
+            return {
+                headerTitle: () => <Header navigation={navigation} title='Pending list' />,
+            }
+        }
+    },
+    ProductDetails: { screen: ProductDetailsScreen }
+}
+
+const ProductListStack = createStackNavigator(screens, {
+    defaultNavigationOptions: {
+        headerTintColor: 'white',
+        headerStyle: {
+            backgroundColor: 'black', height: 90
+        }
+    }
+});
+
+export default ProductListStack;
---
+++ screens/ProductsListScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ProductListScreen() {
+
+    return (
+        
+        <View style={styles.main}>
+                <Text style={ {color:'black'}}>This is Product List Screen </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    main: {
+        height:'100%',
+        width:'100%'
+    },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        // justifyContent: "center",
+        paddingTop:'50%'
+    },
+});
