75df27115e8f7d1d9c16d6f63314ab9091757707
Anand-oops <anand.pandey6599@gmail.com>
2021-02-05 00:33:31 +0530

    Address Functionality Added

--- screens/ProceedToBuy.js
+++ screens/ProceedToBuy.js
@@ -1,150 +1,136 @@
 import React from 'react';
-import { useState,useContext } from 'react';
+import { useState, useContext } from 'react';
 import { AuthContext } from '../navigation/AuthProvider';
-import { StyleSheet, Text, View, FlatList,Image,ScrollView,TextInput } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image, ScrollView, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Firebase from "../firebaseConfig";
+import Toast from 'react-native-simple-toast';
 
 
-export default function ProceedToBuy(props) {
+export default function ProceedToBuy( props ) {
 
-    // const { user } = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
 
-    // console.log("props",props);
-    // const [listen, setListen] = useState(true);
-    // const [items, setItem] = useState([]);
-    // Firebase.database().ref(`Customers/${user.uid}/wishlist`).on('value', (data) => {
-    //     if (listen) {
-    //         if(data.val()){
-    //             var temp = [];
-    //         var keys = Object.keys(data.val());
-    //         console.log('keys',keys);
-                
-    //             for (var i = 0; i < keys.length; i++) {
-    //                 var key = keys[i]
-    //                 temp.push(data.val()[key])
-    //             }
+    const [name, setName] = useState('');
+    const [call, setCall] = useState(true);
+    const [mobile, setMobile] = useState();
+    const [pincode, setPincode] = useState();
+    const [add1, setAdd1] = useState('');
+    const [add2, setAdd2] = useState('');
+    const [landmark, setLandmark] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [addresses, setAddresses] = useState([])
 
-    //             console.log("sddssdsdd",temp);
+    Firebase.database().ref(`Customers/${user.uid}/Address`).once('value').then(data => {
+        if (call) {
+            if (data.val()) {
+                setAddresses(data.val());
+                setCall(false);
+            }
+        }
+    })
 
-    //         setListen(false);
-
-    //         setItem(temp);
-
-    //         console.log('items',items);
-    //         console.log('vsfhbf',temp[0].productName);
-    //     }
-    // }
-        
-    // })
-    // const itemsPress=(item)=>{
-    //     console.log("clicked");
-    //    props.navigation.navigate('ProductDetailsScreen',{item:item});
-    // // console.log(props.navigation.navigate);
-    // }
+    const saveChanges = () => {
+        const address = {
+            name: name,
+            mobile: mobile,
+            pincode: pincode,
+            addressLine1: add1,
+            addressLine2: add2,
+            landmark: landmark,
+            city: city,
+            state: state
+        }
+        const temp = [address, ...addresses]
+        console.log("Adress", temp)
+        Firebase.database().ref(`Customers/${user.uid}/Address`).set(temp).then(props.navigation.goBack())
+    }
 
     return (
 
         <View style={styles.main}>
-            <Text style={{fontSize:22,fontWeight:'bold'}}> Add a new address </Text>
-            
+            <Text style={{ fontSize: 22, fontWeight: 'bold', textAlign: 'center', marginTop: 10 }}> Add a new address </Text>
+
             <ScrollView style={{ marginTop: 10 }}>
-                        
-                            <View >
-                                <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                <TextInput
-                                    placeholder='Country'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)}
-                                     />
-                                </View>
-                                
-                                <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Full Name'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Mobile Number'
-                                    placeholderTextColor='gray'
-                                    keyboardType='number-pad'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='PIN CODE'
-                                    placeholderTextColor='gray'
-                                    keyboardType='number-pad'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Flat,House no.,Building,Company,Apartment'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Area,Colony,Street,Sector,Village'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Landmark e.g. near Apoolo hospital'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='Town/City'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                                    <View style={{borderRadius:1,elevation:1,margin:8,padding:8}}>
-                                    <TextInput
-                                    placeholder='State'
-                                    placeholderTextColor='gray'
-                                    // style={styles.textInput}
-                                    // value={productName}
-                                    // onChangeText={(val) => setProductName(val)} 
-                                    />
-                                    </View>
-                            </View>
-                            <TouchableOpacity >
-                    <View style={{borderRadius:3,elevation:1,margin:6,padding:4,backgroundColor:'#f4a460',height:40,justifyContent:'center'}}>
-                        <Text style={{alignSelf:'center',fontWeight:'bold'}}>SAVE CHANGES</Text>
+
+                <View >
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Full Name'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setName(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Mobile Number'
+                            placeholderTextColor='gray'
+                            keyboardType='number-pad'
+                            maxLength={10}
+                            onChangeText={(val) => setMobile(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='PIN CODE'
+                            placeholderTextColor='gray'
+                            keyboardType='number-pad'
+                            maxLength={6}
+                            onChangeText={(val) => setPincode(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Flat,House no.,Building,Company,Apartment'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setAdd1(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Area,Colony,Street,Sector,Village'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setAdd2(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Landmark e.g. near Apoolo hospital'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setLandmark(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='Town/City'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setCity(val)}
+                        />
+                    </View>
+                    <View style={{ borderRadius: 1, elevation: 1, margin: 8, padding: 8 }}>
+                        <TextInput
+                            placeholder='State'
+                            placeholderTextColor='gray'
+                            onChangeText={(val) => setState(val)}
+                        />
+                    </View>
+                </View>
+                <TouchableOpacity onPress={() => {
+                    if (name.length > 0 && mobile.length >= 10 && pincode.length == 6 && add1.length > 0 && add2.length > 0
+                        && landmark.length > 0 && city.length > 0 && state.length > 0) {
+                        saveChanges();
+                    } else
+                        Toast.show("Fill all fields", Toast.SHORT);
+                }}>
+                    <View style={{ borderRadius: 3, elevation: 1, margin: 6, padding: 4, backgroundColor: '#f4a460', height: 40, justifyContent: 'center' }}>
+                        <Text style={{ alignSelf: 'center', fontWeight: 'bold' }}>SAVE CHANGES</Text>
                     </View>
                 </TouchableOpacity>
-                        
-                    </ScrollView>
 
-                
+            </ScrollView>
+
+
         </View>
     );
 }
@@ -159,7 +145,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         paddingTop: '50%'
     },
-    text:{
-        color:'blue'
+    text: {
+        color: 'blue'
     }
 });
