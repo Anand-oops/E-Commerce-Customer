ad51e3e995250227c2d12cfd23bf4ff461c6d8d7
Anand-oops <anand.pandey6599@gmail.com>
2021-02-07 01:47:32 +0530

    Ratings done

--- screens/WriteReview.js
+++ screens/WriteReview.js
@@ -1,74 +1,107 @@
 import React from 'react';
-import { useState, useContext, useRef } from 'react';
+import { useState, useContext } from 'react';
 import { AuthContext } from '../navigation/AuthProvider';
-import { StyleSheet, Text, View, Image, Alert, TouchableOpacity ,TextInput} from 'react-native';
-import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, Image, TouchableOpacity, TextInput } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import StarRating from "react-native-star-rating";
-import { AntDesign } from '@expo/vector-icons';
 import Firebase from "../firebaseConfig";
-import Counter from "react-native-counters";
 import Toast from 'react-native-simple-toast';
-import RBSheet from 'react-native-raw-bottom-sheet';
-import { CheckBox } from 'react-native-elements'
 
 export default function WriteReview(props) {
 
     const { user } = useContext(AuthContext);
-const [desReview,setdesReview]=useState('');
-const [titleReview,setTitleReview]=useState('');
-    const [item,setItem]=useState(props.route.params.item);
-    const [rate,setRating]=useState(0);
-    const Rating=(rating)=>{
-        setRating(rating);
-        console.log("rating",rating);
+    const [call, setCall] = useState(true)
+    const [desReview, setdesReview] = useState('');
+    const [titleReview, setTitleReview] = useState('');
+    const [item] = useState(props.route.params.item);
+    const [rate, setRating] = useState(0);
+    const [updateCheck, setUpdateCheck] = useState(true);
+    const [orgRating, setOrgRating] = useState(0);
+    const [count, setCount] = useState(0)
 
-    }
-    const submitChanges=()=>{
-        console.log("des review",desReview);
-        console.log('title rev',titleReview);
-        console.log('rating',rate);
-        var rev ={
-          revId:user.uid,
-          revTitle:titleReview,
-          revDesc:desReview
+    Firebase.database().ref(`ProductList/${item.category}/${item.subCategory}/${item.key}/Reviews`).once('value').then(data => {
+        if (call) {
+            if (data.val()) {
+                // console.log(data.val())
+                setCount(Object.keys(data.val()).length)
+                var item = data.val()[user.uid];
+                setTitleReview(item.revTitle);
+                setdesReview(item.revDesc);
+                setRating(item.revRating);
+                setOrgRating(item.revRating)
+                setUpdateCheck(false);
+            }
+            setCall(false);
         }
+    })
 
-        // Firebase.database().ref(`Customers/${user.uid}/wishlist`).set(rev).then(()=>{
-        //     Toast.show("Review Submitted",Toast.SHORT);
-        // })
+    const submitChanges = () => {
+        console.log('rating', rate);
+        console.log('orgRate', orgRating);
+        console.log('count', count);
+        var rev = {
+            revId: user.uid,
+            revTitle: titleReview,
+            revDesc: desReview,
+            revRating: rate,
+        }
+        Firebase.database().ref(`ProductList/${item.category}/${item.subCategory}/${item.key}/Reviews/${user.uid}`).set(rev).then(() => {
+            Firebase.database().ref(`ProductList/${item.category}/${item.subCategory}/${item.key}/rating`).transaction(function (currentRating) {
+                if (updateCheck) {
+                    if (currentRating == 0)
+                        return rate;
+                    else
+                        return (currentRating + rate) / 2;
+                }
+                else {
+                    if (orgRating > rate)
+                        return currentRating + (rate - orgRating) / count;
+                    else if (orgRating < rate)
+                        return currentRating + (rate - orgRating) / count;
+                    else
+                        return currentRating;
+                }
+
+
+            }).then(() => {
+                Toast.show("Review Submitted", Toast.SHORT);
+                props.navigation.goBack();
+            })
+
+        })
     }
 
 
     return (
         <ScrollView>
             <View style={styles.main}>
-                
-                    <View style={{ margin: 4, borderColor: 'white', borderRadius: 1, elevation: 1, flexDirection: 'row' }}>
-                        <View style={{ borderColor: 'white', borderRadius: 1, elevation: 1, flex: 1 }}>
-                            <Image
-                                style={{ padding: 2, height: 150, width: '98%', resizeMode: 'stretch', alignSelf: 'center', }}
-                                source={{ uri: item.image.uri }}
-                            />
-                        </View>
-                        <View style={{ flex: 1 }}>
-                            <Text style={{ color: '#3b3a30', fontSize: 20, padding: 4, textTransform: 'capitalize' }}>{item.productName}</Text>
-                <StarRating
-                   disabled={false}
-                   maxStars={5}
-                   rating={rate}
-                   starSize={30}
-                   fullStarColor={'#ffa500'}
-                   emptyStarColor={'#ff4500'}
-                   selectedStar={(rating)=>{Rating(rating)}}                
-                />
-                <TouchableOpacity onPress={()=>{setRating(0)}} style={{elevation:1,borderRadius:1,width:50,margin:8,backgroundColor:'coral'}}>
-                    <Text style={{alignSelf:'center',fontWeight:'bold'}}>Clear</Text>
-                </TouchableOpacity>
-                            
-                        </View>
+
+                <View style={{ margin: 4, borderColor: 'white', borderRadius: 1, elevation: 1, flexDirection: 'row' }}>
+                    <View style={{ borderColor: 'white', borderRadius: 1, elevation: 1, flex: 1 }}>
+                        <Image
+                            style={{ padding: 2, height: 150, width: '98%', resizeMode: 'stretch', alignSelf: 'center', }}
+                            source={{ uri: item.image.uri }}
+                        />
+                    </View>
+                    <View style={{ flex: 1 }}>
+                        <Text style={{ color: '#3b3a30', fontSize: 20, padding: 4, textTransform: 'capitalize' }}>{item.productName}</Text>
+                        <StarRating
+                            disabled={false}
+                            maxStars={5}
+                            rating={rate}
+                            starSize={30}
+                            fullStarColor={'#ffa500'}
+                            emptyStarColor={'#ff4500'}
+                            selectedStar={(rating) => { setRating(rating) }}
+                        />
+                        <TouchableOpacity onPress={() => { setRating(0) }} style={{ elevation: 1, borderRadius: 1, width: 50, margin: 8, backgroundColor: 'coral' }}>
+                            <Text style={{ alignSelf: 'center', fontWeight: 'bold' }}>Clear</Text>
+                        </TouchableOpacity>
 
                     </View>
-                
+
+                </View>
+
                 <View
                     style={{
                         marginTop: 5,
@@ -79,18 +112,18 @@ const [titleReview,setTitleReview]=useState('');
 
                 <View>
                     <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold', marginTop: 6, marginHorizontal: 4 }}>Add a Descriptive Review</Text>
-                    <View  style={{height:200,marginHorizontal:8,marginVertical:6,borderRadius:1,elevation:1}}>
-                        <TextInput 
-                        placeholder={"What did you like or dislike? Did this product fulfilled your requirements?"}
-                        multiline={true}
-                        style={{height:40,margin:6,color:'grey'}}
-                        value={desReview}
-                        onChangeText={(text)=>setdesReview(text)}
+                    <View style={{ height: 200, marginHorizontal: 8, marginVertical: 6, borderRadius: 1, elevation: 1 }}>
+                        <TextInput
+                            placeholder={"What did you like or dislike? Did this product fulfilled your requirements?"}
+                            multiline={true}
+                            style={{ height: 40, margin: 6, color: 'grey' }}
+                            value={desReview}
+                            onChangeText={(text) => setdesReview(text)}
                         />
                     </View>
-                            
-                        
-                    
+
+
+
                 </View>
                 <View
                     style={{
@@ -100,17 +133,17 @@ const [titleReview,setTitleReview]=useState('');
                     }}
                 />
                 <View>
-                <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold', marginTop: 6, marginHorizontal: 4 }}>Add a Title for your Review</Text>
-                    <View  style={{height:100,marginHorizontal:8,marginVertical:6,borderRadius:1,elevation:1}}>
-                        <TextInput 
-                        style={{height:40,marginHorizontal:6,color:'grey'}}
-                        placeholder={'Sum up your review in one line.'}
-                        value={titleReview}
-                        onChangeText={(text)=>{setTitleReview(text)}}
+                    <Text style={{ color: 'black', fontSize: 20, fontWeight: 'bold', marginTop: 6, marginHorizontal: 4 }}>Add a Title for your Review</Text>
+                    <View style={{ height: 100, marginHorizontal: 8, marginVertical: 6, borderRadius: 1, elevation: 1 }}>
+                        <TextInput
+                            style={{ height: 40, marginHorizontal: 6, color: 'grey' }}
+                            placeholder={'Sum up your review in one line.'}
+                            value={titleReview}
+                            onChangeText={(text) => { setTitleReview(text) }}
 
                         />
                     </View>
-                    
+
 
                 </View>
                 <View
@@ -122,8 +155,13 @@ const [titleReview,setTitleReview]=useState('');
                 />
 
             </View>
-            <TouchableOpacity onPress={()=>{submitChanges()}} style={{backgroundColor:'coral',height:40,borderRadius:1,elevation:1,marginHorizontal:4,marginVertical:5,justifyContent:'center'}}>
-                <Text style={{alignSelf:'center',fontSize:20,fontWeight:'bold'}}>Submit</Text>
+            <TouchableOpacity onPress={() => {
+                if (rate != 0) {
+                    submitChanges();
+                } else
+                    Toast.show("Rate the product atleast", Toast.SHORT);
+            }} style={{ backgroundColor: 'coral', height: 40, borderRadius: 1, elevation: 1, marginHorizontal: 4, marginVertical: 5, justifyContent: 'center' }}>
+                <Text style={{ alignSelf: 'center', fontSize: 20, fontWeight: 'bold' }}>Submit</Text>
             </TouchableOpacity>
 
 
@@ -133,7 +171,7 @@ const [titleReview,setTitleReview]=useState('');
 
 const styles = StyleSheet.create({
     main: {
-        flex:1
+        flex: 1
     },
 
 
--- screens/YourOrders.js
+++ screens/YourOrders.js
@@ -3,18 +3,18 @@ import { useState, useContext } from 'react';
 import { AuthContext } from '../navigation/AuthProvider';
 import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import Firebase from "../firebaseConfig";
-import { FlatList, LongPressGestureHandler, ScrollView } from "react-native-gesture-handler";
+import { FlatList, ScrollView } from "react-native-gesture-handler";
 import Toast from 'react-native-simple-toast'
 
 
-export default function YourOrders({navigation}) {
+export default function YourOrders({ navigation }) {
 
     const { user } = useContext(AuthContext);
 
     const [listen, setListen] = useState(true);
     const [orders, setOrders] = useState([])
 
-    Firebase.database().ref(`Customers/${user.uid}/Orders`).on('value',data => {
+    Firebase.database().ref(`Customers/${user.uid}/Orders`).on('value', data => {
         if (listen) {
             if (data.val()) {
                 var list = [];
@@ -30,31 +30,31 @@ export default function YourOrders({navigation}) {
         }
     });
 
-    const pressHandler=(item)=>{
+    const pressHandler = (item) => {
         console.log("clickedd");
-   navigation.navigate('ReviewScreen',{item:item});
+        navigation.navigate('ReviewScreen', { item: item });
     }
 
     return (
 
         <View style={styles.main}>
-            <ScrollView style={{ flex: 1, marginTop:10 }}>
+            <ScrollView style={{ flex: 1, marginTop: 10 }}>
                 <FlatList data={orders}
                     renderItem={data => (
-                        <TouchableOpacity onPress={()=>pressHandler(data.item)}>
-                        <View style={styles.listContainer}>
-                            
-                            <Image source={data.item.image} style={styles.listimage} />
-                            <View style={styles.list}>
-                                <Text style={{ color: 'black', fontWeight: 'bold' }}>Order Id: {data.item.orderId}</Text>
-                                <Text style={{ color: 'black' }}>Name : {data.item.productName}</Text>
-                                <Text style={{ color: 'purple' }}>Category : {data.item.category} :: {data.item.subCategory}</Text>
-                                <Text style={{ color: 'blue' }}>Price: {data.item.productPrice}</Text>
-                                <Text style={{ color: 'black' }}>Address: {data.item.address.city + "," + data.item.address.state + " - " + data.item.address.pincode}</Text>
-                                <Text style={{ color: 'red' }}>{data.item.deliveryStatus}</Text>
+                        <TouchableOpacity onPress={() => pressHandler(data.item)}>
+                            <View style={styles.listContainer}>
+
+                                <Image source={data.item.image} style={styles.listimage} />
+                                <View style={styles.list}>
+                                    <Text style={{ color: 'black', fontWeight: 'bold' }}>Order Id: {data.item.orderId}</Text>
+                                    <Text style={{ color: 'black' }}>Name : {data.item.productName}</Text>
+                                    <Text style={{ color: 'purple' }}>Category : {data.item.category} :: {data.item.subCategory}</Text>
+                                    <Text style={{ color: 'blue' }}>Price: {data.item.productPrice}</Text>
+                                    <Text style={{ color: 'black' }}>Address: {data.item.address.city + "," + data.item.address.state + " - " + data.item.address.pincode}</Text>
+                                    <Text style={{ color: 'red' }}>{data.item.deliveryStatus}</Text>
+                                </View>
+
                             </View>
-                            
-                        </View>
                         </TouchableOpacity>
                     )} />
             </ScrollView>
@@ -86,8 +86,7 @@ const styles = StyleSheet.create({
     listimage: {
         height: 10,
         width: 10,
-        padding: 20,
-        
+        padding: 40,
         marginHorizontal: 20,
     },
 });
